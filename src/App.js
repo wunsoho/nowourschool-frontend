3a0d63006d848d2d81f89f308d3394ea9d531d3a
Zzang9u <146842085+Zzang9u@users.noreply.github.com>
2024-01-22 14:28:07 +0000

    reset

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import logo from './logo.svg';
 import './App.css';
-import Categories from "./components/Categories/Categories";
-import Study from "./components/Categories/Study";
-import Semina from "./components/Categories/Semina";
-import Sportfacilities from "./components/Categories/SportFacilities";
-import CultureSpace from "./components/Categories/CultureSpace";
-import PrinterPC from "./components/Categories/PrinterPC";
-import PopularFacility from "./components/PopularFacilities/PopularFacility";
-import NavigationBar from "./components/NavigationBar/NavigationBar";
-import Slide from "./components/Slide/Slide";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Categories/>
-        <Routes>
-          <Route path="/study" element={<Study/>}/>
-          <Route path="/semina" element={<Semina/>}/>
-          <Route path="/sportfacilities" element={<Sportfacilities/>}/>
-          <Route path="/culturespace" element={<CultureSpace/>}/>
-          <Route path="/printerpc" element={<PrinterPC/>}/>
-        </Routes>
-        <PopularFacility/>
-        <Slide/>
-        <NavigationBar/>
-      </BrowserRouter>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
