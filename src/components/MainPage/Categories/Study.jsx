cca4e87584372f132b1049c9b4389fd72b1f0df4
Zzang9u <146842085+Zzang9u@users.noreply.github.com>
2024-02-05 06:36:54 +0000

    스터디 터치 시 나오는 화면 구현2

--- src/components/MainPage/Categories/Study.jsx
+++ src/components/MainPage/Categories/Study.jsx
@@ -6,18 +6,32 @@ import OpenClass from "../Image/OpenClass.png";
 function Study(){
 
     return(
-        <div>
+        <div style={{marginBottom:"10vh"}}>
             <div style={{paddingTop:"2vh"}}>
                 <Link to="/">
                     <img src={hwasalpyo2} alt="화살표" style={{float:"left",margin:"0",marginTop:"2.5vh", marginLeft:"2vw"}}/>
                 </Link>
                 <h3 style={{display:"flex",justifyContent:"center",marginRight:"6vw",fontWeight:"700",fontSize:"20px",letterSpacing:"-0.4vw"}}>스터디</h3>
             </div>
-            {/* ui 구현은 했는데 스크롤로 했을 때 글자가 안뜨는 것을 고쳐야됨 */}
             <div style={{display:"flex", marginBottom:"2vh"}}>
                 <img src={OpenClass} alt="열린 열람실" style={{width:"96vw", position:"relative",verticalAlign:"middle"}}/>
-                <p style={{position:"absolute",top:"32.3vh",margin:"0",marginLeft:"2vw",color:"white",letterSpacing:"-0.3vw",fontWeight:"600",fontSize:"16px"}}>독서실 분위기의 공부공간</p>
-                <p style={{position:"absolute",top:"31vh",right:"5vw",margin:"0",color:"white",fontWeight:"800",fontSize:"28px",letterSpacing:"-0.3vw"}}>열린 열람실</p>
+                <p style={{position:"absolute",margin:"0",marginTop:"22.5vh",marginLeft:"2vw",color:"white",letterSpacing:"-0.3vw",fontWeight:"600",fontSize:"16px"}}>독서실 분위기의 공부공간</p>
+                <p style={{position:"absolute",margin:"0",marginTop:"21vh",marginLeft:"58.5vw",color:"white",fontWeight:"800",fontSize:"28px",letterSpacing:"-0.3vw"}}>열린 열람실</p>
+            </div>
+            <div style={{display:"flex", marginBottom:"2vh"}}>
+                <img src={OpenClass} alt="열린 열람실" style={{width:"96vw", position:"relative",verticalAlign:"middle"}}/>
+                <p style={{position:"absolute",margin:"0",marginTop:"22.5vh",marginLeft:"2vw",color:"white",letterSpacing:"-0.3vw",fontWeight:"600",fontSize:"16px"}}>독서실 분위기의 공부공간</p>
+                <p style={{position:"absolute",margin:"0",marginTop:"21vh",marginLeft:"58.5vw",color:"white",fontWeight:"800",fontSize:"28px",letterSpacing:"-0.3vw"}}>열린 열람실</p>
+            </div>
+            <div style={{display:"flex", marginBottom:"2vh"}}>
+                <img src={OpenClass} alt="열린 열람실" style={{width:"96vw", position:"relative",verticalAlign:"middle"}}/>
+                <p style={{position:"absolute",margin:"0",marginTop:"22.5vh",marginLeft:"2vw",color:"white",letterSpacing:"-0.3vw",fontWeight:"600",fontSize:"16px"}}>독서실 분위기의 공부공간</p>
+                <p style={{position:"absolute",margin:"0",marginTop:"21vh",marginLeft:"58.5vw",color:"white",fontWeight:"800",fontSize:"28px",letterSpacing:"-0.3vw"}}>열린 열람실</p>
+            </div>
+            <div style={{display:"flex", marginBottom:"2vh"}}>
+                <img src={OpenClass} alt="열린 열람실" style={{width:"96vw", position:"relative",verticalAlign:"middle"}}/>
+                <p style={{position:"absolute",margin:"0",marginTop:"22.5vh",marginLeft:"2vw",color:"white",letterSpacing:"-0.3vw",fontWeight:"600",fontSize:"16px"}}>독서실 분위기의 공부공간</p>
+                <p style={{position:"absolute",margin:"0",marginTop:"21vh",marginLeft:"58.5vw",color:"white",fontWeight:"800",fontSize:"28px",letterSpacing:"-0.3vw"}}>열린 열람실</p>
             </div>
         </div>
     );
