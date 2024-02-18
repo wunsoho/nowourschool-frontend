fcb1cad4ec6b5f8ea2eae8d848ad21166af3f59e
Zzang9u <146842085+Zzang9u@users.noreply.github.com>
2024-02-18 15:47:16 +0000

    메인화면 카테고리 api 연동

--- src/components/MainPage/Categories/SportFacilities.jsx
+++ src/components/MainPage/Categories/SportFacilities.jsx
@@ -33,8 +33,20 @@ function SportFacilities(){
       }, []);
 
     return(
-        <div>
-            <p>체육시설</p>
+        <div style={{marginBottom:"10vh"}}>
+            <div style={{paddingTop:"0.5vh"}}>
+                <Link to="/">
+                    <img src={hwasalpyo2} alt="화살표" style={{float:"left",margin:"0",marginTop:"2.5vh", marginLeft:"2vw"}}/>
+                </Link>
+                <h3 style={{display:"flex",justifyContent:"center",marginRight:"6vw",fontWeight:"700",fontSize:"20px",letterSpacing:"-0.4vw"}}>체육시설</h3>
+            </div>
+            {facilityData.map((fd)=>(
+                <div style={{display:"flex", marginBottom:"2vh"}}>
+                    <img src={fd.imageURL} alt={fd.name} style={{width:"96vw", position:"relative",verticalAlign:"middle"}}/>
+                    <p style={{position:"absolute",margin:"0",marginTop:"22.5vh",marginLeft:"2vw",color:"white",letterSpacing:"-0.3vw",fontWeight:"600",fontSize:"16px"}}>{fd.description}</p>
+                    <p style={{position:"absolute",margin:"0",marginTop:"21vh",marginLeft:"58.5vw",color:"white",fontWeight:"800",fontSize:"28px",letterSpacing:"-0.3vw"}}>{fd.name}</p>
+                </div>
+            ))}
         </div>
     );
 }
